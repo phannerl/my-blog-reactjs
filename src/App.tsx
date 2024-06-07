1f5a8b2247b9b07beaac3b81a1f0ac7431903dc8
phannerl <36044017+phannerl@users.noreply.github.com>
2024-06-07 10:24:48 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Home Page
+      </div>
+      <div>
+        List blogs
+      </div>
+    </>
+  )
+}
+
+export default App
