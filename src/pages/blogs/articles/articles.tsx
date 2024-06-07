093da78e288fd581574a6d6722500c2ba650853e
phannerl <36044017+phannerl@users.noreply.github.com>
2024-06-08 04:08:59 +0700

    feat: Add Articles details page

--- src/pages/blogs/articles/articles.tsx
+++ src/pages/blogs/articles/articles.tsx
@@ -1,5 +1,33 @@
+import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../../../redux';
+import { FaRegClock } from 'react-icons/fa';
+import parse from 'html-react-parser';
+
 export const Articles = () => {
+    const { id } = useParams();
+    const article = useAppSelector((state) =>
+        state.articles.find((article) => article.id === id!),
+    );
+    if (!article) {
+        return <div>Article not found</div>;
+    }
     return (
-        <div>Articles Page</div>
+        <div className='text-left'>
+            <h1>{article.title}</h1>
+            <div className='mb-2'>
+                <FaRegClock className="mr-1" />
+                <small>
+                    {new Date(article.createdAt).toLocaleTimeString('en-US', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    })}
+                </small>
+            </div>
+            <p>{parse(article.content || "")}</p>
+            <img src={article.image} alt={article.title} className="w-100" />
+        </div>
     );
 };
